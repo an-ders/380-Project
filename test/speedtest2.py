19af18c7f84c51fdffeaf40d9e6aa19270b33ba9
Zara Syed <zsyed350@gmail.com>
2025-03-20 16:35:52 -0400

    speedtest2

---
+++ test/speedtest2.py
@@ -0,0 +1,25 @@
+import sys
+sys.path.append('..')
+from hardware import *
+
+MIN_SPEED = 0.25
+MAX_TEST_SPEED = MAX_SPEED*0.7
+NUM_TESTS = 3
+STEP_SIZE = (MAX_TEST_SPEED-MIN_SPEED)/NUM_TESTS
+
+def adjust_speed_right(speed):
+    multiplier = 0.14*speed + 0.937
+    speed *= multiplier
+    return speed
+
+if __name__ == "__main__":
+    speed_list = [MIN_SPEED + i * STEP_SIZE for i in range(NUM_TESTS+1)]
+    for sp in speed_list:
+        print(sp)
+        drive_motors(sp, sp)
+        while (right_motor_distance()<1):
+            continue
+        drive_motors(-sp, -sp)
+        while (right_motor_distance()>0):
+            continue
+        drive_motors(0, 0)
