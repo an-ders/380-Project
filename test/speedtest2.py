e973b001ec541e0d50aa81967063687ec3cad85b
Zara Syed <zsyed350@gmail.com>
2025-03-21 12:48:45 -0400

    improved print statementd

--- test/speedtest2.py
+++ test/speedtest2.py
@@ -1,3 +1,4 @@
+import time
 import sys
 sys.path.append('..')
 from hardware import *
@@ -6,6 +7,7 @@ MIN_SPEED = 0.25
 MAX_TEST_SPEED = MAX_SPEED*0.7
 NUM_TESTS = 3
 STEP_SIZE = (MAX_TEST_SPEED-MIN_SPEED)/NUM_TESTS
+DIST = 1.5
 
 def adjust_speed_right(speed):
     multiplier = 0.14*speed + 0.937
@@ -15,11 +17,23 @@ def adjust_speed_right(speed):
 if __name__ == "__main__":
     speed_list = [MIN_SPEED + i * STEP_SIZE for i in range(NUM_TESTS+1)]
     for sp in speed_list:
-        print(sp)
+        print("Setting duty cycle to %.2f", sp)
+
+        # FORWARD
         drive_motors(sp, sp)
-        while (right_motor_distance()<1):
+        start_time = time.time()
+        while (right_motor_distance()<DIST):
             continue
+        end_time = time.time()
+        print("Actual forward speed: %.2f m/s", DIST/(end_time-start_time))
+
+        # BACKWARD
         drive_motors(-sp, -sp)
+        start_time = time.time()
         while (right_motor_distance()>0):
             continue
+        end_time = time.time()
+        print("Actual backward speed: %.2f m/s", DIST/(end_time-start_time))
+
+        # STOP
         drive_motors(0, 0)
