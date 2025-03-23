5eac545b1c5b0d022737b93b0d4eb39b39a17783
Zara Syed <zsyed350@gmail.com>
2025-03-23 17:37:36 -0400

    new import method in hardware

--- test/speedtest.py
+++ test/speedtest.py
@@ -1,30 +1,29 @@
-import numpy as np
-
 import sys
-sys.path.append('..')
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 from hardware import *
 
 MIN_DUTY_CYCLE = 0.25
-MAX_TEST_SPEED = MAX_DUTY_CYCLE * 0.7
-STEP_SIZE = (MAX_TEST_SPEED - MIN_DUTY_CYCLE) / 5
-
-print(MAX_TEST_SPEED)
+MAX_TEST_SPEED = MAX_DUTY_CYCLE*0.7
+NUM_TESTS = 3
+STEP_SIZE = (MAX_TEST_SPEED-MIN_DUTY_CYCLE)/NUM_TESTS
 
 def adjust_speed_right(speed):
-    multiplier = 0.14 * speed + 0.937
+    multiplier = 0.14*speed + 0.937
     speed *= multiplier
     return speed
 
-speed = MIN_DUTY_CYCLE
-while speed <= MAX_TEST_SPEED + 0.01:
-    print(speed)
-
-    drive_motors(adjust_speed_right(speed), speed)
-    while ((right_motor_distance() > -1.5) and (left_motor_distance() > -1.5)):
-        continue;
-    
-    drive_motors(-adjust_speed_right(speed), -speed)
-    while ((right_motor_distance() < 0) and (left_motor_distance() < 0)):
-        continue;
-    
-    speed += STEP_SIZE
+if __name__ == "__main__":
+    duty_cycles = [MIN_DUTY_CYCLE + i * STEP_SIZE for i in range(NUM_TESTS+1)]
+    for dc in duty_cycles:
+        print(dc)
+        drive_motors(dc, dc)
+        while (right_motor_distance()<1):
+            print(right_motor_distance())
+        stop_motors()
+        sleep(1)
+        drive_motors(-dc, -dc)
+        while (left_motor_distance()>0):
+           print(left_motor_distance())
+        drive_motors(0, 0)
