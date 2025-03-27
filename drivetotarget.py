141681d8c2168c293531d71a2f209b713c353ced
andersyftai@gmail.com <andersyftai@gmail.com>
2025-03-27 03:05:13 -0400

    New scan and working line follow

--- drivetotarget.py
+++ drivetotarget.py
@@ -1,37 +1,13 @@
 import cv2 as cv
-import numpy as np
-from math import log
 from hardware import *
 import platform
 import PID
-from collections import deque
-import matplotlib.pyplot as plt
 
-STARTUP_FRAMES = 15  # number of frames to throw away on startup
-SECTION_OF_FRAME = 0.8  # bottom percentage of frame to declare as regiion of interest
-VERBOSE = False
-DELAY = 1
-MAX_TURNS = 9
-
-MIN_LEN = 0.1  # m
-MAX_LEN = 0.5 # m
-M = (MAX_DUTY_CYCLE-MIN_DUTY_CYCLE)/(MAX_LEN-MIN_LEN)
-B = MAX_DUTY_CYCLE-MAX_LEN*M
-REAL_M = 1
-REAL_B = 0
-BUFFER_CAPACITY = 5
-
-KP = 0.15  # Proportional gain
+KP = 0.0003  # Proportional gain
 KI = 0.0  # Integral gain
-KD = 0.0  # Derivative gain
+KD = 0.00025  # Derivative gain
 
-base_speed = 0.1
-accel = 0.005
-
-error = 0
-integral = 0
-derivative = 0
-previous_error = 0
+base_speed = 0.2
 
 def drive_to_target_main():
     # Initialize webcam
@@ -40,14 +16,14 @@ def drive_to_target_main():
     else:
         cap = cv.VideoCapture(0)
 
-    native_height = 340
-    native_width = 260
+    error = 0
+    derivative = 0
+    previous_error = 0
     
-    pid = PID.PID()
-    target = False
-    got_line = False  # bool on whether or not line is tracked, prevents premature exit of program
-    count = STARTUP_FRAMES
-    while not target:
+    started = False
+    numTurns = 0
+
+    while True:
         ret, frame = cap.read()
         if not ret:
             print("Failed to capture frame")
@@ -59,52 +35,73 @@ def drive_to_target_main():
 
         mask = cv.bitwise_or(mask1, mask2)  # get all red
 
-        c, h = cv.findContours(mask, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+        c, _ = cv.findContours(mask, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
 
         if c:
-            temp = max(c, key=cv.contourArea)
-            mom = cv.moments(temp)
+            x = findMiddle(c)
 
-            if mom["m00"]:
-                x = int(mom["m10"] / mom["m00"])
-                y = int(mom["m01"] / mom["m00"])
+            if x < 0: continue;
 
-            # if base_speed < base_speed_max:
-            #     base_speed = base_speed + accel
+            error = (1280 // 2) - x
             
-            # if base_speed > base_speed_max:
-            #     base_speed = base_speed_max
-
-            error = (native_width // 2) - x
-            integral += error
+            if abs(error) < 50: continue
+            
             derivative = error - previous_error
-
-            if error < 10:
-                continue
             
-            # Calculate control signal
-            signal = (KP * error) + (KI * integral) + (KD * derivative)
-
-            # Update previous error
+            signal = (KP * error) + (KD * derivative)
             previous_error = error
 
-            left_speed = base_speed + signal
-            right_speed = base_speed - signal
-            print(x, y)
-            # print(f"{left_speed:.3f}, {right_speed:.3f}")
+            left_speed = base_speed - signal
+            right_speed = base_speed + signal
+            
+            print(f"{left_speed:.3f}, {right_speed:.3f}")
+            drive_motors(left_speed, right_speed)
 
+        elif started and numTurns == 0:
+            numTurns += 1
+            drive_motors(0.15, -0.15)
+            while True:
+                ret, frame = cap.read()
+                if not ret:
+                    print("Failed to capture frame")
+                    break
+
+                hsv = cv.cvtColor(frame, cv.COLOR_BGR2HSV)  # Convert to HSV color space
+                mask1 = cv.inRange(hsv, RED_HSV_RANGE['lower_red1'], RED_HSV_RANGE['upper_red1'])  # Create masks for red color
+                mask2 = cv.inRange(hsv, RED_HSV_RANGE['lower_red2'], RED_HSV_RANGE['upper_red2'])  # Create masks for red color
+
+                mask = cv.bitwise_or(mask1, mask2)  # get all red
+
+                c, _ = cv.findContours(mask, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+
+                if c:
+                    x = findMiddle(c)
+
+                    if x < 0: continue;
+
+                    error = (1280 // 2) - x
+                    
+                    if abs(error) < 200: break
+            sleep(2)
         else:
-            print("No red line detected")
             stop_motors()
+            break
 
-        # Display the original frame with detected lines    
-
-        # Break loop on user interrupt (e.g., 'q' key press)
         if cv.waitKey(1) & 0xFF == ord('q'):
             break
 
     cap.release()
     cv.destroyAllWindows()
+
+def findMiddle(c):
+    c = max(c, key=cv.contourArea)
+    mom = cv.moments(c)
+
+    if mom["m00"]:
+        started = True
+        return int(mom["m10"] / mom["m00"])
+
+    return -1
 
 if __name__ == "__main__":
     drive_to_target_main()
